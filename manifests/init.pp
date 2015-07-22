05991ddbebf34e018f1394f1d916422b9820a5b3
Derek Warren <warren@sfu.ca>
2015-07-22 09:10:51 -0700

    Fedora 22 irrational behaviour fix
    This fails on a fresh Fedora 22 for no apparent reason. Peace offering
    to systemd.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,14 +1,50 @@
 class autofs {
 
-  service { autofs:
-    enable => true,
-    ensure => running,
-    subscribe => File["/etc/nsswitch.conf"],
+  file { '/etc/nsswitch.conf':
+    owner => 'root',
+    group => 'root',
+    mode  => '0644',
   }
 
-  file { "/etc/nsswitch.conf":
-    owner => "root",
-    group => "root",
-    mode => "0644",
+  case $::operatingsystem {
+    centos, rhel, fedora: {
+      case $::operatingsystemmajrelease {
+        5, 6: {
+          service { autofs:
+            ensure    => running,
+            enable    => true,
+            subscribe => File["/etc/nsswitch.conf"],
+          }
+        }
+  /* But wait! Why do you have to specify the systemd provider here?
+     Shouldn't it Just Work on CentOS 7 and Fedora 22+?
+
+     yes. yes, it should, and it does on CentOS 7. On Fedora 22 it fails
+     miserably:
+
+puppet-agent[22751]: (/Stage[main]/Autofs/Service[autofs]/enable) change from false to true failed: Could not enable autofs: Execution of '/sbin/chkconfig --add autofs' returned 1: error reading information on service autofs: No such file or directory
+
+          Your Earth logic is useless here! Mwahahahaha! */
+
+        default: {
+          service { autofs:
+            provider   => 'systemd',
+            name       => 'nfs-server',
+            ensure     => running,
+            enable     => true,
+            hasrestart => true,
+            hasstatus  => true,
+            subscribe => File["/etc/nsswitch.conf"],
+          }
+        }
+      }
+    }
+    default: {
+      service { autofs:
+        ensure    => running,
+        enable    => true,
+        subscribe => File["/etc/nsswitch.conf"],
+      }
+    }
   }
 }
