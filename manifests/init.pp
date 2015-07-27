fe29eb918b09bb65d192ec543171b512460278af
Adam S <asa188@sfu.ca>
2015-07-27 15:30:09 -0700

    Added support to convert Ubuntu15.04 sysv init.d autofs script to systemd native to allow mandating it starts AFTER sssd

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,24 @@
 class autofs {
 
+  if $::operatingsystem == 'ubuntu' and $::operatingsystemrelease == '15.04' {
+    file { "/etc/init.d/autofs":
+      ensure => absent
+    }
+    file { "/etc/systemd/system/autofs.service":
+      ensure  => present,
+      content => template('autofs/autofs.service.erb'),
+      owner   => 'root',
+      group   => 'root',
+      mode    => "0622",
+      notify  => [ Service['autofs'], Exec['autofs-systemctl-daemon-reload'] ],
+    }
+    exec { 'autofs-systemctl-daemon-reload':
+      command => 'systemctl daemon-reload',
+      path    => [ '/bin', '/sbin', '/usr/bin', '/usr/sbin' ],
+      refreshonly => true,
+    }
+  }
+
   service { autofs:
     enable => true,
     ensure => running,
